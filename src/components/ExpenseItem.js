e5fadcebb3116120cfe15bb4e82903b586f5c13e
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-05-21 16:54:25 +0530

    added data using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 7, 23);
-  const expenseTitle = "car";
-  const expenseAmount = 34.34;
+function ExpenseItem(props) {
+ 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
