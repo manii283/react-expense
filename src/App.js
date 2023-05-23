bc57d40d7eba2286b66761bcccf2236e8d12c80e
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-05-23 14:31:27 +0530

    added ExpenseForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NweExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -25,7 +26,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
